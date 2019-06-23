930749e7daf4e4acc58bebeb97625b9ff0ca1986
sapuri <tappuri.sapuri@gmail.com>
2019-06-23 13:43:04 +0900

    Add fresh search

--- crawler.py
+++ crawler.py
@@ -15,11 +15,18 @@ class OmiaiCrawler:
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(width, height)
 
-    def run(self):
+    def run(self, type: str = '', page_num: int = 1, timeout: int = 180):
+        """
+        :param type: <recommend | login | interests | fresh>
+        :param page_num: number of loading pages
+        :param timeout: timeout time for waiting for login
+        """
         self.driver.get('https://www.omiai-jp.com/search')
         print('Waiting for login...')
-        self.__wait_for_login()
-        self.__load_items()
+        self.__wait_for_login(timeout)
+        self.__select_search_menu(type)
+        self.__close_info_dialog(type)
+        self.__load_items(page_num)
         self.__crawl()
         time.sleep(10)
         self.driver.quit()
@@ -36,8 +43,8 @@ class OmiaiCrawler:
             self.driver.quit()
             quit(1)
 
-    def __load_items(self, page_num: int = 5):
-        for _ in range(0, page_num):
+    def __load_items(self, page_num: int = 1):
+        for _ in range(1, page_num):
             self.__page_down()
             time.sleep(1)
         self.__page_up()
@@ -50,19 +57,33 @@ class OmiaiCrawler:
         self.driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
 
+    def __select_search_menu(self, type: str = ''):
+        if type == 'fresh':
+            self.driver.find_element_by_class_name(
+                'om-button-search-menu').click()
+            menu_id = 'om-search-menu-fresh'
+            menu = self.driver.find_element_by_id(menu_id)
+            menu.find_element_by_class_name('content').click()
+            time.sleep(1)
+
+    def __close_info_dialog(self, type: str = ''):
+        if type == 'fresh':
+            self.driver.find_element_by_id(
+                'om-dialog-fresh-information-close').click()
+
     def __crawl(self):
         for i, item in enumerate(
                 self.driver.find_elements_by_class_name('search-list-item')):
             print(i + 1, item.get_attribute('data-nickname'))
             item.click()
-            time.sleep(0.5)
+            time.sleep(0.3)
             self.driver.back()
-            time.sleep(0.5)
+            time.sleep(0.7)
 
 
 if __name__ == '__main__':
     crawler = OmiaiCrawler()
     try:
-        crawler.run()
+        crawler.run(type='fresh', page_num=5)
     except KeyboardInterrupt:
         quit(0)
